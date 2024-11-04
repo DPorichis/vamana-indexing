6976e4c9e3b8b39e5a1f41c11313b0c912e20377
Dimitrios - Stefanos Porichis <sdi2100159@di.uoa.gr>
2024-11-04 14:42:47 +0200

    main starting point (reading arguments)

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,66 @@
 using namespace std;
 
 
-int main() {
-    // Input data from user's file
+int main(int argc, char* argv[]) {
     
+    string filename;
+    string queriesname;
+    int k;
+    int dimensions;
+    float a;
+    char type;
 
+    if(argc != 6)
+        cerr << "Incorrect arguments. Usage ./bin/project [filename] [data type] [k parameter] [dimensions-parameter] [a-parameter] [queriesfile]" << endl;
+
+    //**** Read arguments ****//
+
+    // Retrive type
+    if(argv[2][0] != 'c' && argv[2][0] != 'i' && argv[2][0] != 'f')
+    {
+        cout << "Argument passed as type (" << argv[2] << ") is not a valid option. (Available options: i, f, c)" << endl;
+        cout << "Reminder: Usage ./bin/project [filename] [k parameter] [dimensions-parameter] [a-parameter] [queriesfile]" << endl;
+        return -1;
+    }
+    type = argv[2][0];
+
+    // Retrive k  
+    try {
+        k = stoi(argv[3]);
+    } catch (const std::invalid_argument&) {
+        cout << "Argument passed as k (" << argv[3] << ") is not a valid integer." << endl;
+        cout << "Reminder: Usage ./bin/project [filename] [k parameter] [dimensions-parameter] [a-parameter] [queriesfile]" << endl;
+        return -1;
+    } catch (const std::out_of_range&) {
+        cout << "Argument passed as k (" << argv[3] << ") is out of range for an integer." << endl;
+        return -1;
+    }
+
+    // Retrive dimensions
+    try {
+        dimensions = stoi(argv[4]);
+    } catch (const std::invalid_argument&) {
+        cout << "Argument passed as dimensions (" << argv[4] << ") is not a valid integer." << endl;
+        cout << "Reminder: Usage ./bin/project [filename] [k parameter] [dimensions-parameter] [a-parameter] [queriesfile]" << endl;
+        return -1;
+    } catch (const std::out_of_range&) {
+        cout << "Argument passed as dimensions (" << argv[4] << ") is out of range for an integer." << endl;
+        return -1;
+    }
+
+    // Retrive a
+    try {
+        float a = std::stof(argv[5]);
+    } catch (const std::invalid_argument&) {
+        cout << "Argument passed as a (" << argv[5] << ") is not a valid float." << endl;
+        cout << "Reminder: Usage ./bin/project [filename] [k parameter] [dimensions-parameter] [a-parameter] [queriesfile]" << endl;
+        return -1;
+    } catch (const std::out_of_range&) {
+        cout << "Argument passed as a (" << argv[5] << ") is out of range for a float." << endl;
+        return -1;
+    }
+
+    //***** Gerasime do your thing *****//
 
     // Create the Vamana Index
 
@@ -14,5 +70,4 @@ int main() {
     //
 
 
-    cout << "Hello DIT Project!\n";
 }
