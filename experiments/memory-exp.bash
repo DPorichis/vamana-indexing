e7f95d280cc9f1eb599c33466be8dfd9e7a6fa21
Dimitrios - Stefanos Porichis <sdi2100159@di.uoa.gr>
2025-01-11 18:25:17 +0200

    new experiments structure

---
+++ experiments/memory-exp.bash
@@ -0,0 +1,27 @@
+config_files=$(find ./config-files-graph/ -type f -name "*.txt")
+
+rm -rf ./output-data/*-memory.txt
+# Loop through each configuration file and run the program 4 times
+for config_file in $config_files; do
+    
+    config_name=$(basename "$config_file" .txt)
+    config_name=${config_name:7}
+
+    config_file_queries="./config-files-query/$(basename "$config_file")"
+
+    
+    for i in {1..4}; do
+        
+        rm ./data/*-graph.bin
+
+        echo "Running with config file: $config_file (iteration $i)"
+        time_output=$(/usr/bin/time -v ../bin/project -config "$config_file" 2>&1)
+        echo "$time_output" | grep "Maximum resident set size" >> "./output-data/output-${config_name}-graph-memory.txt"
+
+        echo "Running queries on the created graph: $config_file_queries"
+        time_output=$(/usr/bin/time -v ../bin/project -config "$config_file" 2>&1)
+        echo "$time_output" | grep "Maximum resident set size" >> "./output-data/output-${config_name}-queries-memory.txt"
+
+
+    done
+done
--- experiments/query-exp.bash
+++ experiments/query-exp.bash
@@ -1,18 +1,21 @@
-config_files=$(find ./config-files-query/ -type f -name "*.txt")
+#### NOT IN USE ####
 
-rm -rf ./output-data/*-queries.txt
 
-# Loop through each configuration file and run the program 4 times
-for config_file in $config_files; do
+# config_files=$(find ./config-files-query/ -type f -name "*.txt")
+
+# rm -rf ./output-data/*-queries.txt
+
+# # Loop through each configuration file and run the program 4 times
+# for config_file in $config_files; do
     
-    config_name=$(basename "$config_file" .txt)
-    config_name=${config_name:7}
+#     config_name=$(basename "$config_file" .txt)
+#     config_name=${config_name:7}
     
-    for i in {1..4}; do
+#     for i in {1..4}; do
         
-        echo "Running with config file: $config_file (iteration $i)"
-        ../bin/project -config "$config_file" >> "./output-data/output-${config_name}-queries.txt"
+#         echo "Running with config file: $config_file (iteration $i)"
+#         ../bin/project -config "$config_file" >> "./output-data/output-${config_name}-queries.txt"
 
-    done
+#     done
 
-done
+# done
