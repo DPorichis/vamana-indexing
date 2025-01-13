e7911cc6dad76b1cba2965c16d752438762bfa44
Gerasimos Bekos <gerasimos.bekos@gmail.com>
2025-01-14 00:38:22 +0200

    thread bash

--- experiments/thread-exp.bash
+++ experiments/thread-exp.bash
@@ -1,32 +1,33 @@
-config_files=$(find ./config-files-graph/ -type f -name "*.txt")
+graph_config_files=$(find ./config-files-thread/ -type f -name "*parallel.txt")
 
 rm -rf ./output-data/*-thread.txt
 threadcounts=(1 2 4 8 16)
 
-# Loop through each configuration file and run the program 4 times
-for config_file in $config_files; do
+for graph_config_file in $graph_config_files; do
     
-    config_name=$(basename "$config_file" .txt)
-    config_name=${config_name:7}
+    graph_config_name=$(basename "$graph_config_file" .txt)
+    graph_config_name=${graph_config_name:7}
 
-    config_file_queries="./config-files-query/$(basename "$config_file")"
+    # Create graphs for each number of threads
+    for threads in "${threadcounts[@]}"; do
+        # Graph creation
+        rm ./data/*-graph.bin
+        echo "Running with config file: $graph_config_file(threads $threads)"   
+        sed -i "s/^threadcount=.*/threadcount=${threads}/" "$graph_config_file"
+        ../bin/project -config "$graph_config_file" >> "./output-data/output-${graph_config_name}-thread.txt"
 
-    
-    # Iterate through the array of threadcount values
-    for threadcount in "${threadcounts[@]}"; do
+        # Queries for the graph
+        query_config_file=$(basename config-"$graph_config_name"-query.txt)
+
         # Update the threadcount parameter in the config file
         # If threadcount exists, replace its value; otherwise, add it at the end
-        if grep -q "^threadcount=" "$config_file"; then
-            sed -i "s/^threadcount=.*/threadcount=$threadcount/" "$config_file"
-        else
-            echo "threadcount=$threadcount" >> "$config_file"
-        fi
-
-        # Print the updated configuration for this iteration
-        echo "Updated 'threadcount' in '$config_file' to '$threadcount'."
         
-        # Optionally, run a command using the updated configuration
-        # Example:
-        # ./run_program --config "$config_file"
+        # Update the threadcount in the config file
+        sed -i "s/^threadcount=.*/threadcount=${threads}/" "./config-files-thread/$query_config_file"
+
+        ../bin/project -config "./config-files-thread/$query_config_file" >> "./output-data/output-${graph_config_name}-thread.txt"
+
+
     done
 done
+rm ./data/*-graph.bin
