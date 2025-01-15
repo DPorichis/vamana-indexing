482a952fa72d8e277a46230e34151b9e1f36abb1
Gerasimos Bekos <gerasimos.bekos@gmail.com>
2025-01-15 13:25:38 +0200

    final-commit

--- experiments/accur-time-1m-exp.bash
+++ experiments/accur-time-1m-exp.bash
@@ -16,14 +16,14 @@ for config_file in $config_files; do
     echo "Running with config file: $config_file"
     ../bin/project -config "$config_file" >> "./m-1/output-1m/output-${config_name}-graph.txt"
 
-    echo "Running queries on the created graph: $config_file_queries"
-    ../bin/project -config "$config_file_queries" >> "./m-1/output-1m/output-${config_name}-queries.txt"
+    # echo "Running queries on the created graph: $config_file_queries"
+    # ../bin/project -config "$config_file_queries" >> "./m-1/output-1m/output-${config_name}-queries.txt"
 
     graph_file=$(ls ./data/*-graph.bin 2>/dev/null)
         
     # Ensure the graph file exists and rename it to avoid overwriting
     if [ -n "$graph_file" ]; then
-        cp "$graph_file" "./graphs/${config_name}-1m-${i}.bin"
+        cp "$graph_file" "./graphs/${config_name}-1m.bin"
     else
         echo "No graph file found for $config_file"
     fi
